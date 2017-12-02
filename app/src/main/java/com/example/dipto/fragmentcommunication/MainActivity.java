56167357e8a2a020a585e0c2a7d8953b5ba8dbba
imtiaz <programmer.imtiaz@gmail.com>
2017-12-02 12:35:53 +0600

    ADD- Catching the button click value in MainActivity

--- app/src/main/java/com/example/dipto/fragmentcommunication/MainActivity.java
+++ app/src/main/java/com/example/dipto/fragmentcommunication/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.dipto.fragmentcommunication;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RadiogroupFragment.ClickListener {
 
     @BindView(R.id.container)
     FrameLayout container;
@@ -22,8 +23,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         RadiogroupFragment radiogroupFragment = new RadiogroupFragment();
+        radiogroupFragment.onAttach(getApplicationContext());
         FragmentTransaction radio_ft = getSupportFragmentManager().beginTransaction();
         radio_ft.replace(R.id.container, radiogroupFragment, "Pizza Fragment");
         radio_ft.commit();
+    }
+
+    @Override
+    public void dataPassingToActivity(String color) {
+        if(color.equals("WHITE")){
+            Log.d("++++MSG++++", "GOT IT 1!!") ;
+        }
+        else if(color.equals("RED")){
+            Log.d("++++MSG++++", "GOT IT 2!!") ;
+        }
+        else if(color.equals("YELLOW")){
+            Log.d("++++MSG++++", "GOT IT 3!!") ;
+        }
     }
 }
